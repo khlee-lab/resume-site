25d60141dcc6a8615ce96c0b2489ce9f82a8a7c7
kyunghyun lee <45004335+khlee-lab@users.noreply.github.com>
2025-05-17 21:26:22 +0900

    fix: support JSX in jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.tsx'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
